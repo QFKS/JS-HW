90db50db36b87e8b2e2491d6b4dac331c80cbfdd
QFKS <111087976+QFKS@users.noreply.github.com>
2022-11-14 00:48:39 +0200

    11

---
+++ JS-HW-9/scr.js
@@ -0,0 +1,119 @@
+window.onload=()=>{
+    // 1) Створити 100 елементів div зі стилями (задані через js стилі) при кліку на елемен, він повинен змінити бекграунд на червоний.
+    
+    let newDiv = new DocumentFragment();
+    let newElement = document.createElement('div')
+    for(i = 0;i < 100; i++){
+        div = document.createElement('div');
+        div.setAttribute('style','width:25px;height:25px;background-color:green;cursor:pointer;');
+        div.classList.add("hundredDiv");
+        newDiv.appendChild(div)
+        newElement.appendChild(div)
+    }
+    newElement.setAttribute('style','display:grid;grid-template-columns: repeat(auto-fill, 40px);grid-template-rows: repeat(auto, 40px);gap:20px;')
+    document.body.appendChild(newElement)
+    
+    let [...divArr] = document.querySelectorAll('.hundredDiv');
+        for( let i =0; i<divArr.length; i++){
+            divArr[i].addEventListener('click',function(){
+                this.style.backgroundColor = 'red';
+            })
+        }
+    
+        // 2) Створити кнопку, по натисканню на кнопку повино зявлятись модальне вікно, в модалці повинна бути кнопка закрити вікно. Під модальним вікном повиний бути блок підкладка при кліку на який модальне вікно закривається
+    
+    let newFragment = new DocumentFragment();
+    let button = document.createElement('button');
+    button.setAttribute('style','margin-top:50px;padding:5px 30px;cursor:pointer;')
+    button.innerHTML = 'Кнопка'
+    button.classList.add("button");
+    document.body.appendChild(button)
+    
+    let windowModal = document.createElement('div');
+    windowModal.classList.add('underModal', 'hide');
+    newFragment.appendChild(windowModal);
+    
+    let modal = document.createElement('div')
+    modal.setAttribute('style','position:fixed;top:50%;width:100%;height:150px;background-color:#FFF8DC;z-index:3;');
+    modal.classList.add('modal', 'hide')
+    
+    let hideButton = document.createElement('button')
+    hideButton.setAttribute('style','border-radius:5px;')
+    hideButton.innerHTML = `<img src="https://pngimage.net/wp-content/uploads/2018/06/белый-крестик-png-4.png" alt="белый крестик png 4" style ='width:20px;height:20px;'></img>`
+    hideButton.classList.add('close');
+    
+    modal.appendChild(hideButton);
+    newFragment.appendChild(modal);
+    
+    document.body.appendChild(newFragment);
+    
+    button.addEventListener('click', close, true);
+    windowModal.addEventListener('click', close, false);
+    hideButton.addEventListener('click', close, false);
+    
+    function close() { 
+        modal.classList.toggle('hide');
+        windowModal.classList.toggle('hide') 
+    } 
+    // 3) Створити слайдер, через 2 секунди в ньому повино мінятись зображення
+    
+    let slider = document.querySelector('.slide');
+    let arr = ['img/1.png','img/2.png','img/3.png','img/4.png'];
+    for(t=0; t<arr.length; t++){
+        div = document.createElement('img');
+        div.setAttribute('src', arr[t]);
+        div.setAttribute('alt','img')
+        slider.appendChild(div)
+    }
+    let offset = 0
+    const slide = document.querySelector('.slide');
+    const nextBtn = document.querySelector('#next');
+    const prevBtn = document.querySelector('#prev');
+    function next(){
+        offset += 300;
+        if(offset > 900){
+            offset = 0
+        }
+        slide.style.left = -offset + 'px';
+    }
+    nextBtn.addEventListener('click', next);
+    
+    function prev(){
+        offset -= 300;
+        if(offset < 0){
+            offset = 900
+        }
+        slide.style.left = -offset + 'px';
+    }
+    prevBtn.addEventListener('click', prev);
+    
+    function autoSlide(){ 
+        setInterval(next,2000) 
+    } 
+    autoSlide()
+    
+    // 4) Створити div  і 3 кнопки ( вперед, назад, вправо, вліво) при кліку на кожну з кнопок змінювати позицію кнопки ( відноно заданого напрямку на 15 рх)
+    moveElement = document.querySelector('.move_element');
+    let upBtn = document.querySelector('#up');
+    let downBtn = document.querySelector('#down');
+    let leftBtn = document.querySelector('#left');
+    let rightBtn = document.querySelector('#right');
+    let xMove = 0;
+    let yMove = 0;
+    upBtn.addEventListener('click', function(){
+        yMove += 15;
+        moveElement.style.bottom = yMove + 'px';
+    });
+    downBtn.addEventListener('click', function(){
+        yMove -= 15;
+        moveElement.style.bottom = yMove + 'px';
+    });
+    leftBtn.addEventListener('click', function(){
+        xMove += 15;
+        moveElement.style.left = -xMove + 'px';
+    });
+    rightBtn.addEventListener('click', function(){
+        xMove -= 15;
+        moveElement.style.left = -xMove + 'px';
+    });
+    }
---
+++ JS-HW6/scr.js
@@ -0,0 +1,94 @@
+// function sum(...arg) {
+//     let sum = 0;
+//     for (let iterator of arg) {
+//         sum+=iterator  
+//     }
+//     return sum;
+// }
+
+// console.log(sum(5,4,9,10))
+
+//2)Створити функцію яка отримує від користувача день народження,місяць, рік і повертає рік народження і якщо сьогодні день народження користувача то вивести привітання
+
+
+ 
+ 
+//№1   1) Створити функцію яка отримує невизначену кількість аргументів і вираховує їх суму 
+
+
+function fun(a, b, c){
+    let normArr = Array.from(arguments);
+    console.log(normArr);
+    normArr.map((a)=>console.log(a*2));
+
+}   
+fun (prompt());
+
+function modernFn(a,...arg){
+    console.log(a,arg);
+    for(const item of arg){
+        console.log(item);
+    }
+}
+
+
+/*№2     2)Створити функцію яка отримує від користувача день народження,місяць, рік і повертає рік народження і якщо сьогодні день народження користувача то вивести привітання
+*/
+function birthday(){
+let day = prompt("Введіть день народження");
+let month = prompt("Введіть місяць народження");
+let year = prompt("Введіть рік народження");
+let birthday = new Date(year, month, day);
+    const today = new Date();
+    if(((birthday.getDay() || birthday.getDay() || birthday.getDay() == today.getDay())) && (birthday.getMonth() == today.getMonth())) {
+        return("Happy Birthday!")
+    }
+    
+}
+console.log(birthday());
+
+
+//3) Створити функцію яка з массиву вибирає всі булеві значення
+const sort = [true, 'false', 111, -0, 'true', false, {a:false}, [true, 'false', false, 'true', true]];
+function bool(arr) {
+    let res = [];
+    let f = [0,1,2]
+    for (let i = 0; i<arr.length;i++) {
+        if(typeof arr[i] === 'boolean' || typeof arr[i].a === 'boolean') {
+            res.push(arr[i]);
+        }
+        else if (typeof arr[i] === 'object') {
+            for(let r = 0; r<10; r++){
+                if(typeof arr[i][r] === 'boolean') {
+                    res.push(arr[i][r])
+                }
+            }
+            
+        }
+    }
+    return res
+}
+
+console.log(bool(sort))
+
+//4) Створити функцію яка повертає сторіччя, функція отримує рік 1810->19 
+
+function century(year) {
+    let str = prompt('year.toString()');
+    let cent = str[0] + str[1];
+    let res = parseInt(cent)
+    return (res+1);
+}
+console.log(century(1810))
+
+// 5) Створити функцію яка повертає скільки днів в цьому місяці а також в наступному  ( В цьому місяці 30 днів в наступному 31 )
+
+function monthDay() {
+    const date = new Date();
+    let month = date.getMonth()+1;
+    switch (month) {
+        case 10: 
+        return "В цьому місяці 30 днів в наступному 31"
+    }
+}
+console.log(monthDay())
