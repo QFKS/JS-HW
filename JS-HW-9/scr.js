c37e10ee8ab345b128898fb59e2d229f098ae3cf
QFKS <111087976+QFKS@users.noreply.github.com>
2022-11-28 23:27:46 +0200

    11

--- JS-HW-9/scr.js
+++ JS-HW-9/scr.js
@@ -1,31 +1,93 @@
 window.onload=()=>{
-    
-// 1) Створити 100 елементів div зі стилями (задані через js стилі) при кліку на елемен, він повинен змінити бекграунд на червоний.
 
-    let newDiv = new DocumentFragment();
-    let newElement = document.createElement('div')
-        for(i = 0;i < 100; i++){
-        div = document.createElement('div');
-        div.setAttribute('style','width 15px;height 15px;background-color:blueviolet;cursor:pointer;');
-        div.classList.add("stoDiv");
-        newDiv.appendChild(div)
-        newElement.appendChild(div)
+    // 1) Створити слайдер, через 2 секунди в ньому повино мінятись зображення
+    
+let slider = document.querySelector('.slide');
+let arr = ['img/1.png','img/2.png','img/3.png','img/4.png'];
+  for(t=0; t<arr.length; t++){
+       div = document.createElement('img');
+       div.setAttribute('src', arr[t]);
+       div.setAttribute('alt','img')
+       slider.appendChild(div)
     }
-        newElement.setAttribute('style','display:grid;grid-template-columns: repeat(auto-fill, 40px);grid-template-rows: repeat(auto, 40px);gap:20px;')
-        document.body.appendChild(newElement)
-    
-    let [...divArr] = document.querySelectorAll('.stoDiv');
-        for( let i =0; i<divArr.length; i++){
-        divArr[i].addEventListener('click',function(){
-        this.style.backgroundColor = 'red';
-            })
-        }
+let offset = 0
+   const slide = document.querySelector('.slide');
+   const nextBtn = document.querySelector('#next');
+   const prevBtn = document.querySelector('#prev');
+ function next(){
+    offset += 300;
+   if(offset > 900){
+       offset = 0
+ }
+    slide.style.left = -offset + 'px';
+ }
+    nextBtn.addEventListener('click', next);
+        
+function prev(){
+    offset -= 300;
+   if(offset < 0){
+    offset = 900
+ }
+   slide.style.left = -offset + 'px';
+ }
+   prevBtn.addEventListener('click', prev);
+        
+function autoSlide(){ 
+  setInterval(next,2000) 
+  } 
+  autoSlide()
+        
+// 2) Створити div  і 3 кнопки ( вперед, назад, вправо, вліво) при кліку на кожну з кнопок змінювати позицію кнопки ( відноно заданого напрямку на 15 рх)
+ moveElement = document.querySelector('.move_element');
+ let upBtn = document.querySelector('#up');
+ let downBtn = document.querySelector('#down');
+ let leftBtn = document.querySelector('#left');
+ let rightBtn = document.querySelector('#right');
+ let xMove = 0;
+ let yMove = 0;
+    upBtn.addEventListener('click', function(){
+     yMove += 15;
+    moveElement.style.bottom = yMove + 'px';
+    });
+   downBtn.addEventListener('click', function(){
+    yMove -= 15;
+    moveElement.style.bottom = yMove + 'px';
+   });
+    leftBtn.addEventListener('click', function(){
+    xMove += 15;
+    moveElement.style.left = -xMove + 'px';
+    });
+   rightBtn.addEventListener('click', function(){
+    xMove -= 15;
+    moveElement.style.left = -xMove + 'px';
+    });
+// 3) Створити 100 елементів div зі стилями (задані через js стилі) при кліку на елемен, він повинен змінити бекграунд на червоний.
 
-// 2) Створити кнопку, по натисканню на кнопку повино зявлятись модальне вікно, в модалці повинна бути кнопка закрити вікно. Під модальним вікном повиний бути блок підкладка при кліку на який модальне вікно закривається
+let fragment = document.createDocumentFragment();
+let body = document.querySelector('body');
+function divs() {
+    let divContain = document.createElement("div");
+    divContain.setAttribute('style', 'display:inline-block; width:50%;');
+    fragment.appendChild(divContain);
+    body.appendChild(fragment);
+    for(let i = 0; i!=100; i++){
+        let div = document.createElement("div");
+        div.setAttribute('style', 'background:violet;display:inline-block; border:0.5px solid black; width:35px; height:20px;')
+        fragment.appendChild(div);
+        divContain.appendChild(fragment);
+        div.onclick = ()=>{
+            div.style.backgroundColor = 'red';
+        }
+    }
+    
+}
+divs()
+
+// 4) Створити кнопку, по натисканню на кнопку повино зявлятись модальне вікно, в модалці повинна бути кнопка закрити вікно. Під модальним вікном повиний бути блок підкладка при кліку на який модальне вікно закривається
     
     let newFragment = new DocumentFragment();
     let button = document.createElement('button');
-        button.setAttribute('style','margin-top:50px;padding:5px 30px;cursor:pointer;')
+        button.setAttribute('style','display: block; margin-top:60px;padding:1px 30px;cursor:pointer;')
         button.innerHTML = 'Кнопка'
         button.classList.add("button");
     document.body.appendChild(button)
@@ -39,7 +101,7 @@ window.onload=()=>{
         modal.classList.add('modal', 'hide')
     
     let hideButton = document.createElement('button')
-        hideButton.setAttribute('style','border-radius:2px;border-color:green;cursor:pointer;')
+        hideButton.setAttribute('style','border-radius:2px;background:blue;border-color:green;cursor:pointer;')
         hideButton.innerHTML = `<img src="img/4.png" alt="img4" style ='width:20px;height:20px;'></img>`
         hideButton.classList.add('close');
     
@@ -55,66 +117,7 @@ window.onload=()=>{
     function close() { 
         modal.classList.toggle('hide');
         windowModal.classList.toggle('hide') 
+
     } 
-    // 3) Створити слайдер, через 2 секунди в ньому повино мінятись зображення
-    
-    let slider = document.querySelector('.slide');
-    let arr = ['img/1.png','img/2.png','img/3.png','img/4.png'];
-    for(t=0; t<arr.length; t++){
-        div = document.createElement('img');
-        div.setAttribute('src', arr[t]);
-        div.setAttribute('alt','img')
-        slider.appendChild(div)
-    }
-    let offset = 0
-    const slide = document.querySelector('.slide');
-    const nextBtn = document.querySelector('#next');
-    const prevBtn = document.querySelector('#prev');
-    function next(){
-        offset += 300;
-        if(offset > 900){
-            offset = 0
-        }
-        slide.style.left = -offset + 'px';
-    }
-    nextBtn.addEventListener('click', next);
-    
-    function prev(){
-        offset -= 300;
-        if(offset < 0){
-            offset = 900
-        }
-        slide.style.left = -offset + 'px';
-    }
-    prevBtn.addEventListener('click', prev);
-    
-    function autoSlide(){ 
-        setInterval(next,2000) 
-    } 
-    autoSlide()
-    
-    // 4) Створити div  і 3 кнопки ( вперед, назад, вправо, вліво) при кліку на кожну з кнопок змінювати позицію кнопки ( відноно заданого напрямку на 15 рх)
-    moveElement = document.querySelector('.move_element');
-    let upBtn = document.querySelector('#up');
-    let downBtn = document.querySelector('#down');
-    let leftBtn = document.querySelector('#left');
-    let rightBtn = document.querySelector('#right');
-    let xMove = 0;
-    let yMove = 0;
-    upBtn.addEventListener('click', function(){
-        yMove += 15;
-        moveElement.style.bottom = yMove + 'px';
-    });
-    downBtn.addEventListener('click', function(){
-        yMove -= 15;
-        moveElement.style.bottom = yMove + 'px';
-    });
-    leftBtn.addEventListener('click', function(){
-        xMove += 15;
-        moveElement.style.left = -xMove + 'px';
-    });
-    rightBtn.addEventListener('click', function(){
-        xMove -= 15;
-        moveElement.style.left = -xMove + 'px';
-    });
+
     }
